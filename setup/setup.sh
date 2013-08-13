8cd76d8cff805302dfd0d425098c49ed0ce84aa2
touchdown <yutaoz@gmail.com>
2013-08-13 14:01:52 -0400

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -49,7 +49,7 @@ sudo apt-get update
 # Install a special package
 sudo apt-get install -y python-software-properties python g++ make
 # Add a new repository for apt-get to search
-#sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-add-repository ppa:chris-lea/node.js
 # Update apt-get's knowledge of which packages are where
 #sudo apt-get update
 # Now install nodejs and npm
